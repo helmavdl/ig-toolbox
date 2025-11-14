1fb863cd263256c971c64b9870d8505c3fc3b659
Helma van der Linden <helma.van.der.linden@vzvz.nl>
2025-11-14 15:37:59 +0100

    fix: remove the inflate_nictiz-packages script because it's part of the install-fhir-dependencies script

---
+++ toolbox-scripts/install-fhir-dependencies.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Install all dependencies as defined in package.json
+#
+# 2025-11-14 Helma van der Linden
+#
+
+# Location of the script
+ME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# assume we are in <root>/scripts
+ROOT_DIR="$( dirname "$ME_DIR" )"
+
+# date (now)
+DT=$(date +"%Y-%m-%d")
+
+echo "Installing FHIR dependencies..."
+fhir restore
+jq -r '.dependencies | to_entries[] | "\(.key)@\(.value)"' package.json | while read -r package; do 
+	if [ "$${package#nictiz}" != "$${package}" ]; then 
+		echo "Inflating $$package..."; 
+		fhir inflate --package $$package || { echo "Failed to inflate $$package"; exit 1; }; 
+	fi 
+done
+echo "All dependencies installed."
